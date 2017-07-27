a4f39b2e0f5eab8e720b0a3bb0da5e574ec03c28
mohammed ebrahim fitooh eissa <eissa4444@yahoo.com>
2017-07-28 01:07:19 +0200

    implement ios pushing (Apns)

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
-var express = require('express')
-var models = require('../models/models.js');
-var Device = models.Device;
-var router = express.Router();
-var http = require('http');
-var querystring = require('querystring');
+let express = require('express')
+let models = require('../models/models.js');
+let Device = models.Device;
+let router = express.Router();
+let http = require('http');
+let querystring = require('querystring');
+let apns = require("apns")
 
 //register end point  
 router.post('/register/:token', function (req, res, next) {
-    var platform = checkPlatform(req.params.token);
-    var deviceId = "122317498d17";    //device identfier and it should be passed as a parameter
+    let platform = checkPlatform(req.params.token);
+    let deviceId = "122317498d17";    //device identfier and it should be passed as a parameter
 
-    var device = new Device({
+    let device = new Device({
         deviceId: deviceId,
         token: req.params.token,
         platform: platform
@@ -31,12 +32,59 @@ router.post('/register/:token', function (req, res, next) {
 });
 
 router.post('/push/:token', function (req, res, next) {
-    pushNotification(req.params.token,"hello first push",res);
+    pushNotification(req.params.token, "hello first push", res);
 })
 
+let pushNotification = function (token, msg, res) {
+    let apikey = "AIzaSyAcDkr8-aXdZGWzEk2BRCW5ujwjXzEojFw";
+    let iosCertificate =
+        platform = checkPlatform(token);
+    let notification = '';
+    //android
+    if (platform === 1) {
+        let options = {
+            host: 'gcm-http.googleapis.com',
+            path: '/gcm/send',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'key=' + apikey
+            }
+        };
+        notification = querystring.stringify({
+            "notification": {
+                "title": "msg title",
+                "text": msg
+            },
+            "to": token
+        });
+        let post_req = http.request(options, function (res) {
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                console.log('Response: ' + chunk);
+                console.log(res.statusCode);
+            });
+        });
+        // post the data
+        post_req.write(notification);
+        post_req.end();
+        res.json("push success")
+    } else if (platform === 0) {   //IOS
+        let options, connection, notification;
+        options = {
+            keyFile: "",
+            certFile: "certificates/cert.pem",
+            debug: true
+        };
+        connection = new apns.Connection(options);
+        notification = new apns.Notification();
+        notification.device = new apns.Device(token);
+        notification.alert = msg;
+        connection.sendNotification(notification);
+    }
+}
 //0=>ios and 1=>android
-var checkPlatform = function (token) {
-    var token = token.replace(/ /g, '');
+let checkPlatform = function (token) {
+    token = token.replace(/ /g, '');
     if (token.length == 64) {
         if (token.indexOf("-") || token.indexOf("_") == -1) {
             env = 0;
@@ -47,41 +95,6 @@ var checkPlatform = function (token) {
         env = 1;
     }
     return env;
-}
-
-var pushNotification = function (token, msg,res) {
-    apikey = "AIzaSyAcDkr8-aXdZGWzEk2BRCW5ujwjXzEojFw";
-    platform = checkPlatform(token);
-
-    //android
-    if (platform === 1) {
-        var options = {
-            host: 'gcm-http.googleapis.com',
-            path: '/gcm/send',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'key=' + apikey
-            }
-        };
-
-        var notification = querystring.stringify({
-            "notification": {
-                "title": "msg title",
-                "text": msg
-            },
-            "to": token
-        });
-        var post_req = http.request(options, function (res) {
-            res.setEncoding('utf8');
-            res.on('data', function (chunk) {
-                console.log('Response: ' + chunk);
-            });
-        });
-        // post the data
-        post_req.write(notification);
-        post_req.end();
-        res.json("push success")
-    }
 }
 
 module.exports = router;
