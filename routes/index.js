8e58f4214bf23dcfffe851414cc56a872c7f3b7b
mohammed ebrahim fitooh eissa <eissa4444@yahoo.com>
2017-07-27 21:45:27 +0200

    implement pushing notification to android platform

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express')
 var models = require('../models/models.js');
 var Device = models.Device;
 var router = express.Router();
+var http = require('http');
+var querystring = require('querystring');
 
 //register end point  
 router.post('/register/:token', function (req, res, next) {
@@ -15,8 +17,8 @@ router.post('/register/:token', function (req, res, next) {
     });
 
     Device.findOneAndUpdate(
-        { deviceId: deviceId }, 
-        device, 
+        { deviceId: deviceId },
+        device,
         { upsert: true, new: true, runValidators: true },
         function (err, device) {
             if (err) {
@@ -27,6 +29,10 @@ router.post('/register/:token', function (req, res, next) {
         }
     );
 });
+
+router.post('/push/:token', function (req, res, next) {
+    pushNotification(token,"hello first push",res);
+})
 
 //0=>ios and 1=>android
 var checkPlatform = function (token) {
@@ -42,4 +48,40 @@ var checkPlatform = function (token) {
     }
     return env;
 }
+
+var pushNotification = function (token, msg,res) {
+    apikey = "AIzaSyAcDkr8-aXdZGWzEk2BRCW5ujwjXzEojFw";
+    platform = checkPlatform(token);
+
+    //android
+    if (platform === 1) {
+        var options = {
+            host: 'gcm-http.googleapis.com',
+            path: '/gcm/send',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'key=' + apikey
+            }
+        };
+
+        var notification = querystring.stringify({
+            "notification": {
+                "title": "msg title",
+                "text": msg
+            },
+            "to": token
+        });
+        var post_req = http.request(options, function (res) {
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                console.log('Response: ' + chunk);
+            });
+        });
+        // post the data
+        post_req.write(notification);
+        post_req.end();
+        res.json("push success")
+    }
+}
+
 module.exports = router;
